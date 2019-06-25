019029da5cddf1aa640bd7dc4788c6dea4d49f91
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-06-25 15:19:31 -0700

    add grouping column

--- R/gen_csv_data.R
+++ R/gen_csv_data.R
@@ -3,7 +3,7 @@
 #' @param p number of columns
 #' @param n number of rows. Specify only one of n or MB.
 #' @param fname name of the file to save
-#' @param MB size of the data in MB, excluding the group column
+#' @param MB size of the data in MB. An integer group column will make this a little smaller.
 #' @param sampler function that creates random observations
 #' @param prepend_group_column whether to include a 
 #' @param group_probs
@@ -29,7 +29,15 @@ gen_csv_data = function(p, n
 
     d = lapply(seq(p), function(...) sampler(n))
     names(d) = column_names
+    if(prepend_group_column){
+        group_column = sample(length(group_probs), size = n, replace = TRUE, prob = group_probs)
+        group_column = list(group_column)
+        names(group_column) = group_column_name
+        d = c(group_column, d)
+    }
+
     d = do.call(data.frame, d)
+
     data.table:::fwrite(d, fname)
     d
 }
