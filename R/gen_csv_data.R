2815ffd5cf3232b9cbd66875368629a5e7b92d6c
Clark Fitzgerald <clarkfitzg@gmail.com>
2019-06-25 14:28:30 -0700

    first crack implementing data generator

--- R/gen_csv_data.R
+++ R/gen_csv_data.R
@@ -1,19 +1,35 @@
 #' Generate synthetic CSV data
 #'
 #' @param p number of columns
-#' @param n number of rows
-#' @param MB size of the data in MB
+#' @param n number of rows. Specify only one of n or MB.
+#' @param fname name of the file to save
+#' @param MB size of the data in MB, excluding the group column
 #' @param sampler function that creates random observations
 #' @param prepend_group_column whether to include a 
 #' @param group_probs
 #' @param ... arguments to \code{data.table:::fwrite}
-#'
+#' @return d generated data that was written to fname
 gen_csv_data = function(p, n
                         , fname = "~/data/r_data_benchmarks/data.csv"
                         , MB = 100
                         , sampler = runif
                         , group_probs = rep(0.1, 10)
                         , prepend_group_column = TRUE
+                        , column_names = paste0("col", seq(p))
+                        , group_column_name = "g"
                         , ...)
 {
+    if(missing(n)){
+        nsample = 1e3
+        bytes_per_element = round(object.size(sampler(nsample)) / nsample)
+        oneMB = 2^20
+        size_obj_bytes = MB * oneMB
+        n = size_obj_bytes / (p * bytes_per_element)
+    }
+
+    d = lapply(seq(p), function(...) sampler(n))
+    names(d) = column_names
+    d = do.call(data.frame, d)
+    data.table:::fwrite(d, fname)
+    d
 }
